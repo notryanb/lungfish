623c53e73f6322cdb76b8269d813dcb1defa092d
notryanb@gmail.com <notryanb@gmail.com>
2019-06-30 20:46:11 -0400

    start working with askama in generator

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,15 +1,5 @@
-use askama::Template;
 use lungfish::generate;
-
-#[derive(Template)]
-#[template(path = "layout.html")]
-pub struct HelloTemplate<'a> {
-    test_var_1: &'a str,
-}
 
 fn main() {
     generate();
-
-    let hello = HelloTemplate { test_var_1: "this is a temp var" };
-    println!("{}", hello.render().unwrap());
 }
--- src/generator.rs
+++ src/generator.rs
@@ -1,3 +1,4 @@
+use askama::Template;
 use pulldown_cmark::{Parser, Options, html};
 use walkdir::WalkDir;
 
@@ -5,6 +6,13 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::ffi::OsStr;
 use std::path::Path;
+
+
+#[derive(Template)]
+#[template(path = "default.html")]
+pub struct ContentTemplate<'a> {
+    content: &'a str,
+}
 
 pub fn generate() {
     let files = WalkDir::new("./base")
@@ -23,9 +31,11 @@ pub fn generate() {
         let mut html_output = String::new();
         html::push_html(&mut html_output, parser);
 
+        let content = ContentTemplate { content: &html_output };
+
         let html_file_name = Path::new("./build").join(file_path.file_stem().expect("missing file stem")).with_extension("html");
 
         let mut file = File::create(html_file_name).expect("Couldn't create file");
-        file.write_all(&html_output.as_bytes()).expect("couldn't write file");
+        file.write_all(content.render().unwrap().as_bytes()).expect("couldn't write file");
     }
 }
